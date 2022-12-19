aa64532d5795fb3a99b511d51c456adf960f3350
SoegiDev <soegidev.id@gmail.com>
2022-12-19 14:29:45 +0700

    init

--- Datadistribution.py
+++ Datadistribution.py
@@ -1,33 +1,23 @@
-# import numpy
+import numpy
 
-# x = numpy.random.uniform(0.0, 5.0, 250)
+x = numpy.random.uniform(0.0, 5.0, 250)
 
-# print(x)
-
-
-# import numpy
-# import matplotlib.pyplot as plt
-
-# x = numpy.random.uniform(0.0, 5.0, 250)
-
-# plt.hist(x, 5)
-# plt.show()
+print(x)
 
 
-# import numpy
-# import matplotlib.pyplot as plt
+import numpy
+import matplotlib.pyplot as plt
 
-# x = numpy.random.uniform(0.0, 5.0, 100000)
+x = numpy.random.uniform(0.0, 5.0, 250)
 
-# plt.hist(x, 100)
-# plt.show()
+plt.hist(x, 5)
+plt.show()
 
 
-from scipy import stats
+import numpy
+import matplotlib.pyplot as plt
 
-x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = numpy.random.uniform(0.0, 5.0, 100000)
 
-slope, intercept, r, p, std_err = stats.linregress(x, y)
-
-print(r)
+plt.hist(x, 100)
+plt.show()
